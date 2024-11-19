4b884901f35cf83bbacc2a061997d9f253337059
Deepak <102180983+karkideepak@users.noreply.github.com>
2024-11-18 19:22:34 -0600

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,45 @@
+
+//Mon Nov 18
+//Data Types
+
+public class DataType {
+	public static void main(String args[]){
+		
+		// Primitive Data Type
+		byte age = 54;
+		System.out.println("Age is:"+ age);
+
+		short salary = 720;
+		System.out.println("Salary is"+ salary);
+
+		int carPrice = 50000;
+		System.out.println("Car price is" + carPrice);
+
+		long phoneNumber;
+		phoneNumber = 982347564;
+		System.out.println("Phone number is" + phoneNumber);
+		
+		float weight;
+		weight = 152.22f;
+		System.out.println("Weight is" + weight);
+
+		double housePrice = 1099.22d;
+		System.out.println("House price is" + housePrice);
+
+		char gender = 'M';
+		System.out.println("Gender is" + gender);
+
+		boolean isPassed = true;
+		System.out.println("Passed? " + isPassed);
+
+		
+		// Non-Primitive Data Type
+		
+		String name = "Jack";
+		System.out.println("Name is" + name);
+
+		String address = "101 Washington Ave";
+		System.out.println("Address is" + address);
+
+	} 
+}
